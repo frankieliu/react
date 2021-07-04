d9d950b3d450e4c0b5073307e816eae38607183b
Kipling Liu <kipling.i.liu@gmail.com>
2021-07-04 20:32:27 +0000

    Add Containers

--- edel-stein-reich/src/App.js
+++ edel-stein-reich/src/App.js
@@ -2,8 +2,16 @@ import React from "react";
 import Events from "./Events/Events";
 import Card from "./Card/Card";
 import ClickedImage from "./ClickedImage/ClickedImage";
+import Container from "./Container/Container.js";
 
 function App() {
+  /*
+  let l = []
+  for (let i = 1; i < 10; i++) {
+    l.push("/resources/event/edel_0" + i + ".jpg")
+  }
+  */
+
   return (
     <div className="App">
     <Events />
--- edel-stein-reich/src/Container/Container.js
+++ edel-stein-reich/src/Container/Container.js
@@ -1,8 +1,23 @@
 import React from "react";
+import "./Container.css";
 
-function Container() {
+function Container(props) {
   return (
-    <div className="Container">
+    <div className="Container" style={{width:props.width}}>
+      {
+        (() => {
+          let ncols = props.imgs.length
+          let cols = props.imgs.map((s) => {
+            return (
+              <div class="column" style={{flex:100/ncols+"%"}}>
+                <img src={s} style={{width:"100%"}}/>
+              </div>
+            )
+          })
+
+          return cols
+        })()
+      }
     </div>
   );
 }
